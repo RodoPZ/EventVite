21dc4c1cba20fbf65fb24d92a69249cd8e1809b4
RodoPZ <rpinedo2799@gmail.com>
2024-09-03 14:18:59 -0600

    Create NextParty component

---
+++ src/lib/components/NextParty.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div class="flex border-2 border-primary-500 p-2 w-80 items-center">
+	<img class="max-w-32 mr-4" src='{base}/images/spookypeda_banner.jpg' alt='Next event'>
+	<div class='flex-grow'>
+		<div class="flex flex-col gap-2">
+			<h3 class="h3 text-primary-500">Proximamente</h3>
+			<div>
+				<h4 class="h4">Spookypeda Vol.3</h4>
+				<p>25 de octubre</p>
+			</div>
+		</div>
+	</div>
+</div>
