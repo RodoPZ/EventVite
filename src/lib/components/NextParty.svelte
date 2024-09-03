659dffa29d8a41435911c166551404e724f5bf70
RodoPZ <rpinedo2799@gmail.com>
2024-09-03 16:03:51 -0600

    Fix text

--- src/lib/components/NextParty.svelte
+++ src/lib/components/NextParty.svelte
@@ -9,7 +9,7 @@
 	<img class="max-w-32 mr-4" src={src} alt='Next event'>
 	<div class='flex-grow'>
 		<div class="flex flex-col gap-2">
-			<h3 class="h3 text-primary-500">Proximamente</h3>
+			<h3 class="h3 text-primary-500">Próximamente</h3>
 			<div>
 				<h4 class="h4">{name}</h4>
 				<p>{date}</p>
