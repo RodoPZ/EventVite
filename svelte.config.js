4fe4a3f41438e5a7b065636e39735530af490900
RodoPZ <rpinedo2799@gmail.com>
2024-09-02 21:31:43 -0600

    Remove base

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
-const base = dev ? '' : '/EventVite';
+const base = "";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
